839ebe67a6e0c034e915817519af5ea6f7f0cd06
nicewithgreat <1478258383@qq.com>
2019-08-03 22:34:10 +0800

    now:random character generation

--- set1.hs
+++ set1.hs
@@ -5,6 +5,7 @@ module Set1 where
 
 import MCPrelude
 
+--Random Number Generation
 fiveRands :: [Integer]
 fiveRands = loopRandsbynum 5 1
 
@@ -15,3 +16,11 @@ loopRandsbynum :: Integer -> Integer -> [Integer]
 loopRandsbynum 0 _ = []
 loopRandsbynum acc key = let (nextkey,seed) = rand $ mkSeed key
                     in nextkey : loopRandsbynum (acc-1) nextkey
+
+--Random Character Generation
+randLetter :: Seed -> (Char , Seed)
+randLetter seed =   let (key,nextseed) = rand seed
+                    in ((toLetter key),nextseed)
+
+randString3 :: String
+randString3 = foldr ((:).toLetter) [] $ loopRandsbynum 3 1
